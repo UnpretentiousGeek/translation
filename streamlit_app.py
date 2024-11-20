4833dd446ad01a6e9765a945c69a8d7d0de8e03d
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-11-20 06:32:33 +0000

    rtsbgd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,49 @@
 import streamlit as st
 from openai import OpenAI
 
+system_message = '''
+You are bot that will answer questions in the same language in which the question is asked
+
+If the user asks you to do translations work as a interpreter 
+
+'''
 
 if 'client' not in st.session_state:
     st.session_state.client = OpenAI(api_key=st.secrets['openai_key'])
 
-audio_value = st.audio_input("Record a voice message")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = \
+    [{"role": "system", "content": system_message},
+     {"role": "assistant", "content": "How can I help you?"}]
 
-if audio_value:
-    translation = st.session_state.client.audio.transcriptions.create(
+for msg in st.session_state.messages:
+    if msg["role"] != "system":    
+        chat_msg = st.chat_message(msg["role"])
+        chat_msg.write(msg["content"])
+
+if audio_value := st.audio_input("What is up?"):
+    prompt = st.session_state.client.audio.transcriptions.create(
     model="whisper-1", 
     file=audio_value,
     response_format="text"
     )
-    st.write(translation)
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
+    stream = st.session_state.client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=st.session_state.messages
+    )
 
+    response = st.session_state.client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input=stream.choices[0].message.content
+    )
 
+    with st.chat_message("assistant"):
+        response = st.write(stream.choices[0].message.content)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
