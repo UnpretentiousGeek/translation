be4f01bc23a243a9fdba78ad9a10549ddb4c8fe9
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-11-20 04:27:12 +0000

    asvf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+from openai import OpenAI
+
+
+client = OpenAI()
+
+audio_file= open("/path/to/file/audio.mp3", "rb")
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file
 )
+print(transcription.text)
