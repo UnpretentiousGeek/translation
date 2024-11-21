50305bd87d2117285b1aa9dd4e7f133b6fe3bb2d
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-11-21 02:30:55 +0000

    dbfzx

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,38 @@
 import streamlit as st
 from openai import OpenAI
+import base64
+import os
+import datetime
+from io import BytesIO
+from PIL import Image
+
+@st.dialog("Take a Photo")
+def cam():
+    
+    enable = st.checkbox("Enable camera")
+    picture = st.camera_input("Take a picture", disabled=not enable)
+    preprocess(picture)
+
+@st.dialog("upload a file")
+def upl():
+    uploaded_file = st.file_uploader("Upload a photo", type=("jpg", "png"))
+    preprocess(uploaded_file)
+
+
+def preprocess(picture):
+
+    if picture:
+        st.session_state.show_img = picture
+        timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_path = f"image_{timestamp}.png"
+
+        with open(file_path, "wb") as file:
+            file.write(picture.getbuffer())
+
+        with open(file_path, "rb") as image_file:
+             st.session_state.img = base64.b64encode(image_file.read()).decode('utf-8')
+        
+        st.rerun()
 
 system_message = '''
 You are bot that will answer questions in the same language in which the question is asked
@@ -17,10 +50,32 @@ if "messages" not in st.session_state:
     [{"role": "system", "content": system_message},
      {"role": "assistant", "content": "How can I help you?"}]
 
+
+if st.sidebar.button("Camera 📷"):
+    cam()
+
+if st.sidebar.button("Upload files ⬆️"):
+    upl()
+
+if "show_img" in st.session_state:
+    st.sidebar.image(st.session_state.show_img)
+    if st.sidebar.button("Clear ❌"):
+        del st.session_state["img"]
+        del st.session_state["show_img"]
+        st.rerun()
+
 for msg in st.session_state.messages:
-    if msg["role"] != "system":    
-        chat_msg = st.chat_message(msg["role"])
-        chat_msg.write(msg["content"])
+    if msg["role"] != "system":
+        if isinstance(msg["content"], list) and len(msg["content"]) > 1:
+            if msg["content"][1].get("type") == "image_url":
+                col1, col2 = st.columns([1, 3])
+                img_data = base64.b64decode(msg["content"][1]["image_url"]["url"].split(",")[1])
+                col1.image(img_data)
+                chat_msg = st.chat_message(msg["role"]) 
+                chat_msg.write(msg["content"][0].get("text"))
+        else:
+            chat_msg = st.chat_message(msg["role"]) 
+            chat_msg.write(msg["content"])
 
 if audio_value := st.audio_input("What is up?"):
     prompt = st.session_state.client.audio.transcriptions.create(
@@ -28,10 +83,30 @@ if audio_value := st.audio_input("What is up?"):
     file=audio_value,
     response_format="text"
     )
-    st.session_state.messages.append({"role": "user", "content": prompt})
 
-    with st.chat_message("user"):
-        st.markdown(prompt)
+    if "img" in st.session_state:
+        col1, col2 = st.columns([1, 3])
+        img_data = base64.b64decode(st.session_state.img)
+        col1.image(img_data)
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content":[
+        {"type": "text", "text": prompt},
+        {
+        "type": "image_url",
+        "image_url": {
+            "url": f"data:image/jpeg;base64,{st.session_state.img}",
+        },
+        },
+    ]})
+        del st.session_state["img"]
+        del st.session_state["show_img"]
+        
+
+    else:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
 
     stream = st.session_state.client.chat.completions.create(
         model="gpt-4o-mini",
