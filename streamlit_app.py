20b670ba29d178cfb3fdaf7ab8543ea935bc5189
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-12-04 03:13:53 +0000

    kuyhj

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,10 +121,10 @@ else:
     st.sidebar.title(st.session_state.location)
     st.sidebar.image("https://openweathermap.org/img/wn/" + st.session_state.weather["weather"][0]["icon"] + "@2x.png")
     location = (st.session_state.latitude, st.session_state.longitude)
-    m = folium.Map(location=location, zoom_start=40)
+    m = folium.Map(location=location, zoom_start=30)
     folium.Marker(location, popup="Your Location").add_to(m)
     with st.sidebar:
-        folium_static(m, width=200, height=200)
+        folium_static(m, width=250, height=250)
 
     if st.sidebar.button("Camera 📷"):
         cam()
