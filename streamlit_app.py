dd63ee8c19819be3856e9a50a0c1be1c450fce37
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-12-04 03:38:50 +0000

    ed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-Chat_bot = st.Page("Chat_bot.py", title = "Lab 1")
-Currency = st.Page("Currency.py", title = "Lab 2")
+Chat_bot = st.Page("Chat_bot.py", title = "Chat")
+Currency = st.Page("Currency.py", title = "Currency Converter")
 
 pg = st.navigation([Chat_bot, Currency])
 st.set_page_config(page_title="Labs")
